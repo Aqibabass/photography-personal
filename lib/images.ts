b65e7a111e7e6ede2d1f397fcd3e1b4fe4b2b6fb
[Aqib abass] <aqibabass7@gmail.com>
2025-08-25 19:22:47 +0530

    chore: remove unused files and configurations
    - Deleted package.json and related configuration files.
    - Removed API routes for checkout sessions, file downloads, and preview handling.
    - Cleared out unused schemas and types related to Sanity.
    - Removed public assets including images and favicon.
    - Deleted Tailwind CSS and PostCSS configurations.
    - Cleaned up TypeScript configuration and typings.
    - Removed utility functions related to Stripe integration.

--- lib/images.ts
+++
@@ -1,8 +0,0 @@
-import photo1 from '../public/images/photo-1.jpg';
-import photo2 from '../public/images/Post 1-15.jpg';
-import photo3 from '../public/images/work-3-min.jpg';
-import photo4 from '../public/images/work-1-min.jpg';
-import photo5 from '../public/images/work-8-min.jpg';
-
-
-export default [photo1, photo2, photo3, photo4, photo5 ];
--- schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import imageCollection from './imageCollection'
-
-export const schemaTypes = [post, author, category, blockContent, imageCollection]
